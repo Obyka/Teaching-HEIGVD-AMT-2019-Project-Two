ae9b97000c8b8ada8f76e8d898920f9f1676e766
zereddiamond <antoine.hun@bluewin.ch>
2020-01-04 16:56:46 +0100

    Update API specs and implement the two methods for GET all captures

--- project2-api-pokemon/spring-server/src/main/java/ch/heig/amt/pokemon/api/endpoints/CapturesApiControllers.java
+++ project2-api-pokemon/spring-server/src/main/java/ch/heig/amt/pokemon/api/endpoints/CapturesApiControllers.java
@@ -15,17 +15,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.NativeWebRequest;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@Controller
+@RestController
 public class CapturesApiControllers implements CapturesApi {
 
     @Autowired
@@ -47,58 +45,44 @@ public class CapturesApiControllers implements CapturesApi {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    public ResponseEntity<CapturePokemonTrainers> getPokemonWithTrainers(@ApiParam(value = "pokemon ID",required=true) @PathVariable("id_pokemon") Integer idPokemon) {
-        List<Optional<CaptureEntity>> optionalCapturePokemonTrainers = captureRepository.findByidPokemon(idPokemon);
+    public ResponseEntity<CaptureGetAll> getPokemonWithTrainers(@ApiParam(value = "pokemon ID",required=true) @PathVariable("id_pokemon") Integer idPokemon) {
+        List<CaptureEntity>  capturesEntities = captureRepository.findByidPokemon(idPokemon);
 
-        if(optionalCapturePokemonTrainers.isEmpty()) {
-            throw new CaptureNotFoundException("Pokemon with ID : " + idPokemon + " not found.");
+        if(capturesEntities.isEmpty()) {
+            throw new CaptureNotFoundException("Pokemon with ID " + idPokemon + " not found.");
         }
 
-        CapturePokemonTrainers capturePokemonTrainers = new CapturePokemonTrainers();
-        CaptureTrainer captureTrainer = new CaptureTrainer();
+        return ResponseEntity.ok(getAndFetchResults(capturesEntities));
+    }
 
-        for(Optional<CaptureEntity> optionalCapturesEntity : optionalCapturePokemonTrainers) {
-            CaptureEntity captureEntity = optionalCapturesEntity.get();
+    public ResponseEntity<CaptureGetAll> getTrainerWithPokemons(@ApiParam(value = "trainer ID",required=true) @PathVariable("id_trainer") Integer idTrainer) {
+        List<CaptureEntity>  capturesEntities = captureRepository.findByidTrainer(idTrainer);
 
-            captureTrainer.setDateCapture(captureEntity.getDateCapture());
-            captureTrainer.setIdTrainer(captureEntity.getIdTrainer());
-
-            capturePokemonTrainers.setId(captureEntity.getId());
-            capturePokemonTrainers.setIdPokemon(captureEntity.getIdPokemon());
-            capturePokemonTrainers.addTrainersItem(captureTrainer);
+        if(capturesEntities.isEmpty()) {
+            throw new CaptureNotFoundException("Trainer with ID " + idTrainer + " not found.");
         }
 
-        return ResponseEntity.ok(capturePokemonTrainers);
+        return ResponseEntity.ok(getAndFetchResults(capturesEntities));
     }
 
+    private CaptureGetAll getAndFetchResults(List<CaptureEntity> capturesEntities) {
+        CaptureGetAll captureGetAll = new CaptureGetAll();
 
-    @ApiOperation(value = "", nickname = "getTrainerWithPokemons", notes = "Get a specific trainer with his all pokemons", response = CaptureTrainerPokemons.class, tags={  })
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "success", response = CaptureTrainerPokemons.class) })
-    @RequestMapping(value = "/captures/trainers/{id_trainer}",
-            produces = { "application/json" },
-            method = RequestMethod.GET)
-    public ResponseEntity<CaptureTrainerPokemons> getTrainerWithPokemons(@ApiParam(value = "trainer ID",required=true) @PathVariable("id_trainer") Integer idTrainer) {
-        getRequest().ifPresent(request -> {
-            for (MediaType mediaType: MediaType.parseMediaTypes(request.getHeader("Accept"))) {
-                if (mediaType.isCompatibleWith(MediaType.valueOf("application/json"))) {
-                    String exampleString = "null";
-                    ApiUtil.setExampleResponse(request, "application/json", exampleString);
-                    break;
-                }
-            }
-        });
-        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+        List<CaptureGet> captureGetList = new ArrayList<>();
 
+        for(CaptureEntity captureEntity : capturesEntities) {
+            CaptureGet captureGet = new CaptureGet();
+            captureGet.setIdCapture(captureEntity.getId());
+            captureGet.setIdTrainer(captureEntity.getIdTrainer());
+            captureGet.setIdPokemon(captureEntity.getIdPokemon());
+            captureGet.setDateCapture(captureEntity.getDateCapture());
+
+            //captureGetAll.addCapturesItem(captureGet);
+            captureGetList.add(captureGet);
+        }
+
+        captureGetAll.setCaptures(captureGetList);
+        return captureGetAll;
     }
-
-    private CaptureEntity toEntity(Capture capture) {
-        CaptureEntity captureEntity = new CaptureEntity();
-
-        captureEntity.setIdTrainer(capture.getIdTrainer());
-
-        return captureEntity;
-    }
-
 
 }
--- project2-api-pokemon/spring-server/src/main/java/ch/heig/amt/pokemon/repositories/CaptureRepository.java
+++ project2-api-pokemon/spring-server/src/main/java/ch/heig/amt/pokemon/repositories/CaptureRepository.java
@@ -1,6 +1,5 @@
 package ch.heig.amt.pokemon.repositories;
 
-import ch.heig.amt.pokemon.api.model.CapturePokemonTrainers;
 import ch.heig.amt.pokemon.entities.CaptureEntity;
 import org.springframework.data.repository.CrudRepository;
 
@@ -8,5 +7,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface CaptureRepository extends CrudRepository<CaptureEntity, Integer> {
-    List<Optional<CaptureEntity>> findByidPokemon(Integer integer);
+    List<CaptureEntity> findByidPokemon(Integer id);
+
+    List<CaptureEntity> findByidTrainer(Integer id);
 }

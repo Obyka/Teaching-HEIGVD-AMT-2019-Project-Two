36951a06631ddee6f3c31e97a174fe8509cf8e98
zereddiamond <antoine.hun@bluewin.ch>
2020-01-05 16:59:05 +0100

    Add filters in API pokemon and test with and without tocke

---
+++ project2-api-pokemon/spring-server/src/main/java/ch/heig/amt/pokemon/api/util/UtilsJWT.java
@@ -0,0 +1,52 @@
+//source : https://stormpath.com/blog/jwt-java-create-verify
+package ch.heig.amt.pokemon.api.util;
+
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
+import java.security.Key;
+import io.jsonwebtoken.*;
+import java.util.Date;
+
+public class UtilsJWT {
+    private static String SECRET_KEY = "Les carottes sont cuites.";
+    public static Long VALIDITY = 31556952000L;
+    //Sample method to construct a JWT
+    public static String createJWT(String subject, long ttlMillis, boolean isadmin, long iduser) {
+
+        //The JWT signature algorithm we will be using to sign the token
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+        long nowMillis = System.currentTimeMillis();
+        Date now = new Date(nowMillis);
+
+        //We will sign our JWT with our ApiKey secret
+        byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(SECRET_KEY);
+        Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
+
+        //Let's set the JWT Claims
+        JwtBuilder builder = Jwts.builder()
+                .claim("iduser", iduser)
+                .claim("isadmin",isadmin)
+                .setIssuedAt(now)
+                .setSubject(subject)
+                .signWith(signatureAlgorithm, signingKey);
+
+        //if it has been specified, let's add the expiration
+        if (ttlMillis > 0) {
+            long expMillis = nowMillis + ttlMillis;
+            Date exp = new Date(expMillis);
+            builder.setExpiration(exp);
+        }
+
+        //Builds the JWT and serializes it to a compact, URL-safe string
+        return builder.compact();
+    }
+
+    public static Claims decodeJWT(String jwt) {
+        //This line will throw an exception if it is not a signed JWS (as expected)
+        Claims claims = Jwts.parser()
+                .setSigningKey(DatatypeConverter.parseBase64Binary(SECRET_KEY))
+                .parseClaimsJws(jwt).getBody();
+        return claims;
+    }
+}
